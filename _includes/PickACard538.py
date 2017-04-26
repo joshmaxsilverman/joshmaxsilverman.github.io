44bd3c7cab643816c14763d1bd1b29c219eb5656
hectorpefo <hectorpefo@gmail.com>
2017-04-26 10:03:47 -0700

    Add files via upload

--- _includes/PickACard538.py
+++ _includes/PickACard538.py
@@ -1,18 +1,16 @@
-from random import randint,uniform
+from random import randint
 
 num_reps = 1000000
-
 accum = 0
 
-Thresholds = [93,93,92,90,88,86,82,74,55,0]
+Thresholds = [93,92,91,89,87,84,80,72,55,0]
 
 for rep in range(num_reps):
-	Deck = list(range(100))
-	Hold = 100
-	Highest = 0
+	Deck = list(range(100)) # Cards from 0 to 99
+	Hold = 100 # 100 is dummy value until we hold
+	Highest = -1
 	for deal in range(10):
-		card = Deck[randint(0,99-deal)]
-		Deck.remove(card)
+		card = Deck.pop(randint(0,99-deal))
 		if card > Highest:
 			Highest = card
 		if Hold == 100 and card >= Thresholds[deal]:
@@ -21,6 +19,4 @@ for rep in range(num_reps):
 		accum += 1
 
 print(accum/num_reps)
-
-
 
