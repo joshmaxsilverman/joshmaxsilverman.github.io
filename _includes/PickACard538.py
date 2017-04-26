1a6d25938bfb3ed827b208b90e5daff2a483e05c
hectorpefo <hectorpefo@gmail.com>
2017-04-26 10:34:22 -0700

    Update PickACard538.py

--- _includes/PickACard538.py
+++ _includes/PickACard538.py
@@ -7,7 +7,7 @@ Thresholds = [93,92,91,89,87,84,80,72,55,0]
 
 for rep in range(num_reps):
 	Deck = list(range(100)) # Cards from 0 to 99
-	Hold = -1 # 100 is dummy value until we hold
+	Hold = -1 # -1 is dummy value until we hold
 	Highest = -1
 	for deal in range(10):
 		card = Deck.pop(randint(0,99-deal))
